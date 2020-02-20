090c55cf443f4d07759b32b9d526c9c6d49d7a5e
roy-jingyang <7447643+roy-jingyang@users.noreply.github.com>
2020-02-20 14:38:33 +1000

    waiting for windows testing

---
+++ run.bat
@@ -0,0 +1,4 @@
+set FLASK_APP=.\arya\app.py
+set FLASK_DEBUG=1
+python -m flask run --host=0.0.0.0
+
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 export FLASK_APP=./arya/app.py
 export FLASK_DEBUG=1
-$PYTHON -m flask run --host=0.0.0.0
+python -m flask run --host=0.0.0.0
 
